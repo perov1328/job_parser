c7a2a5745d2038955c6a20138cc2b8fca78e663c
Alex_windows <perov97@yandex.ru>
2023-09-27 12:12:36 +0300

    simplification of the main program file

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 
 from src.utils import *
-from src.headhunter import *
-from src.superjob import *
 
 #В хороших программах принято здороваться со всяк туда входящим
 print('''Доброго времени суток!
 Сегодня мы поможем подобрать Вам список подходящих вакансий :)''')
 
+#Обозначения флага для создания "вечного" цикла
+flag = True
+
 #Старт программы
-while True:
+while flag:
 
     user_answer = input('''Выберете необходимое Вам действие:
     1. Поиск вакансий
@@ -19,61 +20,33 @@ while True:
     0. выход
     Ответ: ''')
 
-    if user_answer == '1':
-        #Получение начальных вводных для работы программы
-        site = get_a_site()
-        keyword = get_a_keyword()
-        city = get_a_city()
-        experience = get_experience()
-        count_vacancies = get_count_vacancies()
-        sort_option = get_sort_option()
+    match user_answer:
 
-        #Обработка города в зависимости от выбранного сайта
-        if isinstance(site, HeadHunterAPI):
-            city = HHController.get_city_id(city)
-        elif isinstance(site, SuperJobAPI):
-            city = SJController.validate_city(city)
+        case '1':
+            #Получение списка вакансий и сохранение их в формате JSON
+            search_vacancies()
 
-        #Обработка параметра запроса в зависимости от выбранного сайта
-        if isinstance(site, HeadHunterAPI):
-            sort_option = HHController.order_by(sort_option)
-        elif isinstance(site, SuperJobAPI):
-            sort_option = SJController.order_field(sort_option)
+        case '2':
+            #Получение списка вакансий из файла JSON
+            result = vacancies_list()
+            for el in result:
+                print(el)
+                print('=' * 50)
 
-        #Отправка запроса к сайту с выбранными параметрами запроса
-        if isinstance(site, HeadHunterAPI):
-            response = site.get_vacancies(keyword, city, experience, count_vacancies, sort_option)
-        elif isinstance(site, SuperJobAPI):
-            response = site.get_vacancies(keyword, city, experience, count_vacancies, sort_option)
+        case '3':
+            #Сравнение 2х вакансий
+            print(vacancies_comparison())
 
-        #Сохранение вакансий в файле JSON
-        vacancy_list = []
-        for el in response:
-            vacancy_list.append(el.to_dict())
-        save_to_json('vacancies', vacancy_list)
+        case '4':
+            #Удаление вакансии из списка JSON
+            remove_vacancy()
 
-    elif user_answer == '2':
-        #Получение списка вакансий из файла JSON
-        result = vacancies_list()
-        for el in result:
-            print(el)
-            print('=' * 50)
+        case '5':
+            #Поиск вакансий по ключевым словам
+            print(search_by_criterion())
 
-    elif user_answer == '3':
-        #Сравнение 2х вакансий
-        print(vacancies_comparison())
-
-    elif user_answer == '4':
-        #Удаление вакансии из списка JSON
-        remove_vacancy()
-
-    elif user_answer == '5':
-        #Поиск вакансий по ключевым словам
-        print(search_by_criterion())
-
-    elif user_answer == '0':
-        #Прощание с пользователем и выход
-        print('\nСпасибо что воспользовались моей программой.'
-        '\nP.S. Поддержать разработчика можно переводом на сбер +791696407**'
-        '\nP.S.S. Добавление новых функций и оптимизация запланированы на конец сентября 2023 г.')
-        break
+        case '0':
+            #Прощание с пользователем и выход
+            print('\nСпасибо что воспользовались моей программой.'
+                  '\nВсего доброго!')
+            flag = False
